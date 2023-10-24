6a296ae517fbcb686d4d0bf6dc5280629f6ce7a1
bedirhangull <gu1bedirhann@gmail.com>
2023-10-24 16:57:40 +0300

    fix(readme): readme file changed

--- webpack.config.js
+++ webpack.config.js
@@ -8,39 +8,31 @@ const webpack = require("webpack");
 module.exports = (env, argv) => ({
   mode: argv.mode === "production" ? "production" : "development",
 
-  // This is necessary because Figma's 'eval' works differently than normal eval
   devtool: argv.mode === "production" ? false : "inline-source-map",
 
   entry: {
-    ui: "./src/index.tsx", // The entry point for your UI code
-    code: "./src/code.ts", // The entry point for your plugin code
+    ui: "./src/index.tsx", 
+    code: "./src/code.ts",
   },
 
   module: {
     rules: [
-      // Converts TypeScript code to JavaScript
       {
         test: /\.tsx?$/,
         use: "ts-loader",
         exclude: /node_modules/,
       },
-
-      // Enables including CSS by doing "import './file.css'" in your TypeScript code
       {
         test: /\.css$/,
         include: path.resolve(__dirname, "src"),
         use: ["style-loader", "css-loader", "postcss-loader"],
       },
-      // Allows you to use "<%= require('./file.svg') %>" in your HTML code to get a data URI
-      // { test: /\.(png|jpg|gif|webp|svg|zip)$/, loader: [{ loader: 'url-loader' }] }
       {
         test: /\.(svg|png|gif)/,
         type: "asset/inline",
       },
     ],
   },
-
-  // Webpack tries these extensions for you if you omit the extension like "import './file'"
   resolve: {
     extensions: [".tsx", ".ts", ".jsx", ".js"],
     alias: {
@@ -52,10 +44,9 @@ module.exports = (env, argv) => ({
     publicPath: "",
     assetModuleFilename: "[name][ext]",
     filename: "[name].js",
-    path: path.resolve(__dirname, "dist"), // Compile into a folder called "dist"
+    path: path.resolve(__dirname, "dist"), 
   },
 
-  // Tells Webpack to generate "ui.html" and to inline "ui.ts" into it
   plugins: [
     new webpack.ProvidePlugin({
       Buffer: ["buffer", "Buffer"],
@@ -64,7 +55,7 @@ module.exports = (env, argv) => ({
       process: "process/browser",
     }),
     new webpack.DefinePlugin({
-      global: {}, // Fix missing symbol error when running in developer VM
+      global: {},
     }),
     new HtmlWebpackPlugin({
       inject: "body",
